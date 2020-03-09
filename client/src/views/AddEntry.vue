f242b08ef925ff17c2f0d7e48da2d7fcaa2d4ff8
François Van Eesbeeck <francois@vaneesbeeck.com>
2020-03-09 23:34:47 +0100

    frontend form field validation (add and edit phone book entries)

--- client/src/views/AddEntry.vue
+++ client/src/views/AddEntry.vue
@@ -2,13 +2,13 @@
   <div id="add-entry">
     <navbar view="AddEntry" title="Phone book"></navbar>
     <b-form-group class="form-label-group">
-      <b-form-input id="input-first-name" v-model="firstNameInput" class="form-control" placeholder="First name"></b-form-input>
+      <b-form-input id="input-first-name" v-model="firstNameInput" class="form-control" placeholder="First name" :state="firstNameState" aria-describedby="Required"></b-form-input>
     </b-form-group>
     <b-form-group class="form-label-group">
-      <b-form-input id="input-last-name" v-model="lastNameInput" class="form-control" placeholder="Last name"></b-form-input>
+      <b-form-input id="input-last-name" v-model="lastNameInput" class="form-control" placeholder="Last name" :state="lastNameState" aria-describedby="Required"></b-form-input>
     </b-form-group>
     <b-form-group class="form-label-group">
-      <b-form-input id="input-phone-number" v-model="phoneNumberInput" class="form-control" placeholder="Phone number"></b-form-input>
+      <b-form-input id="input-phone-number" v-model="phoneNumberInput" class="form-control" placeholder="Phone number" :state="phoneNumberState" aria-describedby="Required"></b-form-input>
     </b-form-group>
     <b-button class="submit-btn" @click="submit">Submit</b-button>
   </div>
@@ -22,18 +22,42 @@ export default {
   mixins: [generalMixin],
   data () {
     return {
-      firstNameInput: null,
-      lastNameInput: null,
-      phoneNumberInput: null,
+      firstNameInput: "",
+      lastNameInput: "",
+      phoneNumberInput: "",
+
+      firstNameState: null,
+      lastNameState: null,
+      phoneNumberState: null,
     }
   },
   methods: {
+    setFieldsStates() {
+      if(this.firstNameInput && this.firstNameInput != "")
+        this.firstNameState = true;
+      else
+        this.firstNameState = false;
+      if(this.lastNameInput && this.lastNameInput != "")
+        this.lastNameState = true;
+      else
+        this.lastNameState = false;
+      this.phoneNumberState = this.validPhoneNumber(this.phoneNumberInput);
+    },
+    fieldsValidation() {
+      this.setFieldsStates();
+      return this.firstNameState && this.lastNameState && this.phoneNumberState;
+    },
     submit() {
-      this.$store.dispatch('apiPostPhoneBookEntry', {
-        firstName: this.firstNameInput,
-        lastName: this.lastNameInput,
-        phoneNumber: this.phoneNumberInput
-      });
+      if(this.fieldsValidation()) {
+        this.$store.dispatch('apiPostPhoneBookEntry', {
+          firstName: this.firstNameInput,
+          lastName: this.lastNameInput,
+          phoneNumber: this.phoneNumberInput
+        });
+      }
+    },
+    validPhoneNumber(phoneNumber) {
+      return RegExp(/^[\+]\d{1,} \d{1,} \d{6,}/i).test(phoneNumber);
     }
   }
 }
--- client/src/views/EditEntry.vue
+++ client/src/views/EditEntry.vue
@@ -2,13 +2,13 @@
   <div id="edit-entry">
     <navbar view="EditEntry" title="Phone book"></navbar>
     <b-form-group class="form-label-group">
-      <b-form-input id="input-first-name" v-model="firstNameInput" class="form-control" placeholder="First name"></b-form-input>
+      <b-form-input id="input-first-name" v-model="firstNameInput" class="form-control" placeholder="First name" :state="firstNameState" aria-describedby="Required"></b-form-input>
     </b-form-group>
     <b-form-group class="form-label-group">
-      <b-form-input id="input-last-name" v-model="lastNameInput" class="form-control" placeholder="Last name"></b-form-input>
+      <b-form-input id="input-last-name" v-model="lastNameInput" class="form-control" placeholder="Last name" :state="lastNameState" aria-describedby="Required"></b-form-input>
     </b-form-group>
     <b-form-group class="form-label-group">
-      <b-form-input id="input-phone-number" v-model="phoneNumberInput" class="form-control" placeholder="Phone number"></b-form-input>
+      <b-form-input id="input-phone-number" v-model="phoneNumberInput" class="form-control" placeholder="Phone number" :state="phoneNumberState" aria-describedby="Required"></b-form-input>
     </b-form-group>
     <b-button class="submit-btn" @click="submit">Submit</b-button>
   </div>
@@ -26,6 +26,10 @@ export default {
       firstNameInput: null,
       lastNameInput: null,
       phoneNumberInput: null,
+
+      firstNameState: null,
+      lastNameState: null,
+      phoneNumberState: null,
     }
   },
   mounted() {
@@ -38,13 +42,33 @@ export default {
       this.lastNameInput = entry.lastName;
       this.phoneNumberInput = entry.phoneNumber;
     },
+    setFieldsStates() {
+      if(this.firstNameInput && this.firstNameInput != "")
+        this.firstNameState = true;
+      else
+        this.firstNameState = false;
+      if(this.lastNameInput && this.lastNameInput != "")
+        this.lastNameState = true;
+      else
+        this.lastNameState = false;
+      this.phoneNumberState = this.validPhoneNumber(this.phoneNumberInput);
+    },
+    fieldsValidation() {
+      this.setFieldsStates();
+      return this.firstNameState && this.lastNameState && this.phoneNumberState;
+    },
     submit() {
-      this.apiPutPhoneBookEntry({
-        _id: {$oid:this.id},
-        firstName: this.firstNameInput,
-        lastName: this.lastNameInput,
-        phoneNumber: this.phoneNumberInput
-      });
+      if(this.fieldsValidation()) {
+        this.apiPutPhoneBookEntry({
+          _id: {$oid:this.id},
+          firstName: this.firstNameInput,
+          lastName: this.lastNameInput,
+          phoneNumber: this.phoneNumberInput
+        });
+      }
+    },
+    validPhoneNumber(phoneNumber) {
+      return RegExp(/^[\+]\d{1,} \d{1,} \d{6,}/i).test(phoneNumber);
     }
   }
 }
